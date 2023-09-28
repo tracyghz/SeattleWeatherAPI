28fae29d43eb74cebe30c8bfb4ff0e92abacf5bf
Huizhong Gao <huizhonggao@huizhongs-mbp.lan>
2023-09-28 00:50:53 -0700

    Update test script with more test cases with better logging

--- test_seattle_weather_api.py
+++ test_seattle_weather_api.py
@@ -1,26 +1,57 @@
+import logging
 import requests
 
-# Define the base URL of your API
-base_url = 'http://localhost:443'  # Update with the correct URL
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+# create console handler and set level to info
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+# add console handler to logger
+logger.addHandler(console_handler)
 
-# Define the API endpoint you want to test
-endpoint = '/query'
+# API base url and endpoints
+BASE_URL = 'http://localhost:443'
+GET_ENDPOINT = '/get-all'
+QUERY_ENDPOINT = '/query'
 
-# Define query parameters (if needed)
-params = {
+# Define query parameters
+limit_weather_query_params = {
     'limit': 5,
     'weather': 'rain'
 }
+limit_date_query_params = {
+    'limit': 10,
+    'date': '2012-01-08'
+}
 
-# Send a GET request to the API
-response = requests.get(base_url + endpoint, params=params)
+query_params = [limit_weather_query_params, limit_date_query_params]
 
-# Check the response status code
-if response.status_code == 200:
-    # The request was successful
-    data = response.json()  # Parse the JSON response
-    print('API Response:', data)
-else:
-    # There was an error
-    print('API Error - Status Code:', response.status_code)
-    print('Response Content:', response.text)
+
+def test_get_all():
+    api = BASE_URL + GET_ENDPOINT
+    logger.info(f'Testing API: {api}')
+    response = requests.get(api)
+    if response.status_code == 200:
+        data = response.json()
+        logger.info(f'API Response: {data}\n test_get_all is successful!')
+    else:
+        logger.error(f'API Error - Status Code: {response.status_code}')
+        logger.error(f'Response Content: {response.text}')
+
+
+def test_query():
+    api_base = BASE_URL + QUERY_ENDPOINT
+    logger.info(f'Testing API: {api_base}')
+    for params in query_params:
+        response = requests.get(api_base, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            logger.info(f'API Response: {data}\n test_query is successful!')
+        else:
+            logger.error(f'API Error - Status Code: {response.status_code}')
+            logger.error(f'Response Content: {response.text}')
+
+if __name__ == '__main__':
+    test_query()
+    test_get_all()
